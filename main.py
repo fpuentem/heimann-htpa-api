45b7b3b74c628edc4d82438d60108158643dd8e3
Elf Golbin <elf@smtp.zoho.com>
2020-11-23 03:08:12 -0300

    main file, initial test

---
+++ main.py
@@ -0,0 +1,28 @@
+# main.py
+
+# Standard library imports
+import time
+import csv
+from datetime import datetime
+
+# Third party imports
+from flask import Flask, render_template, Response, json
+
+# Local application imports
+from libs.htpa import *
+
+
+app = Flask(__name__)
+
+# Thermal camera driver
+dev = HTPA(0x1A)
+
+@app.route('/home')
+def home():
+    return "<h1>Face Enrolling - VisionTech</h1>"
+
+
+
+if __name__ == '__main__':
+    # defining server ip address and port
+    app.run(host='0.0.0.0',port='5000', debug=True)
